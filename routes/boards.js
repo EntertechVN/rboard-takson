cc887622204c7738d6f53e8e04a4fdd87eacae8a
Jared Chu <me@jaredchu.com>
2018-07-16 00:48:50 +0700

    .

--- routes/boards.js
+++ routes/boards.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('boards')
 });
 
+router.get('/history', function(req, res, next) {
+  res.render('boards-history')
+});
+
 module.exports = router;
